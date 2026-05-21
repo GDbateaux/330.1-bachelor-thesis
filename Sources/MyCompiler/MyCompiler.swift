12d9db8e044bb68b39d54ff8f5da2b80a48dc722
GDbateaux <yolan.savioz@hes-so.ch>
2026-05-21 11:54:02 +0200

    feat: Add lexer

---
+++ Sources/MyCompiler/MyCompiler.swift
@@ -0,0 +1,8 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+@main
+struct MyCompiler {
+    static func main() {
+        
+    }
+}
---
+++ Sources/MyCompiler/Parser/Token.swift
@@ -0,0 +1,5 @@
+public struct Token {
+    public let type: TokenType
+    public let lexeme: String
+    public let line: Int
+}
